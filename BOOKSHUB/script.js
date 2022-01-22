334d2e9de5f6faa6fa1fbb69557172e98777f25c
ansh3038 <96246964+ansh3038@users.noreply.github.com>
2022-01-22 08:03:44 +0530

    Add files via upload

---
+++ BOOKSHUB/script.js
@@ -0,0 +1,9 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+function change() {
+    if (document.getElementById('search').value !== '') {
+        document.getElementById('cross').style.color = "white";
+
+    }
+}
