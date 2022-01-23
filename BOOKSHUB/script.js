280a1f88ccd98f3621641e14f709db7cdc180500
ansh3038 <96246964+ansh3038@users.noreply.github.com>
2022-01-23 19:42:01 +0530

    Delete BOOKSHUB directory

--- BOOKSHUB/script.js
+++
@@ -1,9 +0,0 @@
-function myFunction(x) {
-    x.classList.toggle("change");
-}
-function change() {
-    if (document.getElementById('search').value !== '') {
-        document.getElementById('cross').style.color = "white";
-
-    }
-}
